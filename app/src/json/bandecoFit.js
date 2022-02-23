e843a0c20d87040ee12d212dee9cc887675bfb57
AmigoDroid <canalmeuovo36@gmail.com>
2022-02-23 13:13:33 -0300

    coresion

--- app/src/json/bandecoFit.js
+++ app/src/json/bandecoFit.js
@@ -4,7 +4,7 @@ const bandecoFit =[
 
     {'nome':'Filé de Frango grelhado','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
-    {'nome':'Empanado de Frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, salada e batata frinta empanada','valor':preco},
+    {'nome':'Empanado de Frango integral','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, salada e batata frinta empanada','valor':preco},
 
 
     {'nome':'Strogonoff de frango integral','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
