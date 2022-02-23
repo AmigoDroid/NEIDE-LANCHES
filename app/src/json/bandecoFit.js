5b95c7affbe69c1d517b56b91a4985a27eeae396
AmigoDroid <canalmeuovo36@gmail.com>
2022-02-23 12:54:01 -0300

    macarrao integral

--- app/src/json/bandecoFit.js
+++ app/src/json/bandecoFit.js
@@ -1,14 +1,14 @@
 const preco='R$: 15,00'
 const bandecoFit =[
-    {'nome':'Omelete','descrision':'<strong>Acompanhamento:</strong><br> arroz integral, macarrão,purê de batata e salada crua','valor':preco},
+    {'nome':'Omelete','descrision':'<strong>Acompanhamento:</strong><br> arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
-    {'nome':'Filé de Frango grelhado','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão,purê de batata e salada crua','valor':preco},
+    {'nome':'Filé de Frango grelhado','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
-    {'nome':'Strogonoff de frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão,purê de batata e salada crua','valor':preco},
+    {'nome':'Strogonoff de frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
-    {'nome':'Panqueca de frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão, batata grelhada e salada crua','valor':''},
+    {'nome':'Panqueca de frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, batata grelhada e salada crua','valor':''},
 
-    {'nome':'Almondega de carne','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão, purê de batata e salada crua','valor':''}
+    {'nome':'Almondega de carne','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, purê de batata e salada crua','valor':''}
 
 
 ];
