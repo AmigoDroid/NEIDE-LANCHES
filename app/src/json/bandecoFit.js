50913527a2a7d10e9bfce057d303cda7d8b89874
AmigoDroid <canalmeuovo36@gmail.com>
2022-02-23 13:12:30 -0300

    empanado de frango add

--- app/src/json/bandecoFit.js
+++ app/src/json/bandecoFit.js
@@ -4,6 +4,9 @@ const bandecoFit =[
 
     {'nome':'Filé de Frango grelhado','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
+    {'nome':'Empanado de Frango','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, salada e batata frinta empanada','valor':preco},
+
+
     {'nome':'Strogonoff de frango integral','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral,purê de batata e salada crua','valor':preco},
 
     {'nome':'Panqueca de frango integral','descrision':'<strong>Acompanhamento:</strong><br>  arroz integral, macarrão integral, batata grelhada e salada crua','valor':''},
