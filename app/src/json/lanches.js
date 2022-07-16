32e6505a80f1138212e4fc06334c026c2b00d288
AmigoDroid <canalmeuovo36@gmail.com>
2022-07-16 16:56:01 -0300

    TESTE

--- app/src/json/lanches.js
+++ app/src/json/lanches.js
@@ -1,18 +1,18 @@
 const lanches = [
     {'nome':'Suco','descrision':'','valor':'R$: 5,00'},
-    {'nome':'Refrigerante','descrision':'latinha de 250mL','valor':'R$: 5,00'},
+    {'nome':'Refrigerante','descrision':'latinha de 3 50mL','valor':'R$: 5,00'},
     {'nome':'Refrigerante','descrision':'Garafa de 1L','valor':'R$: 8,00'},
     {'nome':'Salada de fruta','descrision':'','valor':'R$: 6,00'},
     {'nome':'Pastel de Forno','descrision':'','valor':'R$: 5,00'},
     {'nome':'Sanduíche americano','descrision':'Pão de forma, patê de frango, milho, ervilha, batata palha e tomate','valor':'R$: 10,00'},
 
-{'nome':'Dogão','descrision':'Pão, salsicha, carne moída, molho barbecue, salada, milho, ervilha e batata palha.','valor':'R$: 8,00'},
+{'nome':'Dogão quente especial','descrision':'Pão, salsicha, carne moída, molho barbecue, salada, milho, ervilha e batata palha.','valor':'R$: 8,00'},
 
 {'nome':'Cachorro quente especial','descrision':'Pão, salsicha, carne moída, molho barbecue, salada com tomate, queijo mussarela, queijo cheddar, calabresa, ovo , milho, ervilha e batata palha.','valor':'R$: 15,00'},
 
 {'nome':'Lasanha de frango','descrision':'Frango, presunto, queijo mussarela, e massa de lasanha.','valor':'R$: 15,00'},
 
-{'nome':'Torta de forno','descrision':'Pão de forma, frango, queijo mussarela, presunto, milho, ervilha, tomate, cebola, pimentão e orégano.','valor':'R$: 10,00'},
+{'nome':'Torta de forno','descrision':'Pão de forma, frango, queijo mussarela, presunto, milho, ervilha, tomate, cebola, pimentão e orégano.','valor':'R$: 8,00'},
 
 {'nome':'Coxinha','descrision':'Frango com queijo mussarela, calabresa com queijo ou presunto com queijo.','valor':'R$: 5,00'},
 
@@ -20,7 +20,7 @@ const lanches = [
 
 {'nome':'Enrolado de salsicha','descrision':'','valor':'R$: 3,00'},
 
-{'nome':'Torta fria','descrision':'Pão de forma, patê de frango, requeijão, creme de leite, milho, ervilha e azeitona.','valor':'R$: 8,00'},
+{'nome':'Torta fria','descrision':'Pão de forma, patê de frango, requeijão, creme de leite, milho, ervilha e azeitona.','valor':'R$: 10,00'},
 
 {'nome':'Macarronada','descrision':'Carne com salsicha, barbecue, molho de tomate, milho, ervilha e azeitona.','valor':'R$: 8,00'},
 
@@ -28,7 +28,7 @@ const lanches = [
 
 {'nome':'Panquecas','descrision':'Frango ou carne moída e queijo mussarela.','valor':'R$: 10,00'},
 
-{'nome':'Strogonoff de frango','descrision':'com arroz branco e batata palha','valor':'R$: 8,00'},
+{'nome':'Strogonoff de frango','descrision':'com arro    branco e batata palha','valor':'R$: 8,00'},
 
 {'nome':'vatapá de camarão','descrision':'com arroz branco','valor':'R$: 12,00'}
 
