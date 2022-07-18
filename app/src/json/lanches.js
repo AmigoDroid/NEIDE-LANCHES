4db339391bc7f56dfc86a522fe75b48415f81513
AmigoDroid <canalmeuovo36@gmail.com>
2022-07-18 19:29:28 -0300

    chama

--- app/src/json/lanches.js
+++ app/src/json/lanches.js
@@ -6,11 +6,13 @@ const lanches = [
     {'nome':'Pastel de Forno','descrision':'','valor':'R$: 5,00'},
     {'nome':'Sanduíche americano','descrision':'Pão de forma, patê de frango, milho, ervilha, batata palha e tomate','valor':'R$: 10,00'},
 
-{'nome':'DogãoS especial','descrision':'Pão, salsicha, carne moída, molho barbecue, salada, milho, ervilha e batata palha.','valor':'R$: 15,00'},
+{'nome':'Dogão Normal','descrision':'Pão, salsicha, carne moída, molho barbecue, salada, milho, ervilha e batata palha.','valor':'R$: 12,00'},
 
 {'nome':'Lasanha de frango','descrision':'Frango, presunto, queijo mussarela, e massa de lasanha.','valor':'R$: 15,00'},
 
 {'nome':'Torta de forno','descrision':'Pão de forma, frango, queijo mussarela, presunto, milho, ervilha, tomate, cebola, pimentão e orégano.','valor':'R$: 8,00'},
+
+{'nome':'Dogão especial','descrision':'Pão, salsicha, carne moída,ovo,calabresa,presunto e queijo','valor':'R$: 15,00'},
 
 {'nome':'Coxinha','descrision':'Frango com queijo mussarela, calabresa com queijo ou presunto com queijo.','valor':'R$: 5,00'},
 
