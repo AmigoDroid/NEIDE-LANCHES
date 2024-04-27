4cdd89581a1c998c9802773bd4325e4746f28cfd
= <=>
2024-04-27 15:44:59 -0300

    atualização cardapio

--- app/src/json/lanches.js
+++ app/src/json/lanches.js
@@ -5,39 +5,65 @@ const lanches = [
         'valor':'R$: 10,00'
     },
     {
-        'nome':'Refrigerante',
-        'descrision':'latinha de 290mL',
-        'valor':'R$: 5,00'
+        'nome':'Burguer x salada',
+        'descrision':'Pão,queijo mussarela, presunto,tomate, pepino, alface ,milho, ervilha, batata palha.',
+        'valor':'R$: 10,00'
     },
-    {'nome':'Refrigerante','descrision':'Garafa de 1L','valor':'R$: 8,00'},
-    {'nome':'Salada de fruta','descrision':'','valor':'R$: 6,00'},
-    {'nome':'Pastel de Forno','descrision':'','valor':'R$: 5,00'},
-    {'nome':'Sanduíche americano','descrision':'Pão de forma, patê de frango, milho, ervilha, batata palha e tomate','valor':'R$: 10,00'},
+    {'nome':'Burguer especial',
+    'descrision':'pão,Carne, ovo, queijo mussarela, presunto,tomate, pepino,  alface, milho e ervilha,  batalha palha, molho caseiro',
+    'valor':'R$: 16,00'},
+    {'nome':'SUPER BURGUER',
+    'descrision':'pão, Carne, ovo,presunto, queijo mussarela, calabresa,bacon,tomate, pepino,  alface, milho e ervilha,  batalha palha, molho caseiro ',
+    'valor':'R$: 20,00'},
 
-{'nome':'Dogão Normal','descrision':'Pão, salsicha, carne moída, molho barbecue, salada, milho, ervilha e batata palha.','valor':'R$: 8,00'},
+    {'nome':'X MEGA BURGUER ',
+    'descrision':'pão, 2 Carnes, 2 ovos,presunto, queijos mussarela, calabresa,bacon,alface,pepino,tomate,milho, ervilha,  batalha palha, molho caseiro',
+    'valor':'R$: 32,00'},
 
-{'nome':'Lasanha de frango','descrision':'Frango, presunto, queijo mussarela, e massa de lasanha.','valor':'R$: 15,00'},
+    {'nome':'Cachorro quente simples',
+    'descrision':' pão,meia salsicha,carne,molho, salada, milho, ervilha,  batalha palha',
+    'valor':'R$: 6,00'},
 
-{'nome':'Torta de forno','descrision':'Pão de forma, frango, queijo mussarela, presunto, milho, ervilha, tomate, cebola, pimentão e orégano.','valor':'R$: 8,00'},
-
-{'nome':'Dogão especial','descrision':'Pão, salsicha, carne moída,ovo,calabresa,presunto e queijo','valor':'R$: 15,00'},
-
-{'nome':'Coxinha','descrision':'Frango com queijo mussarela, calabresa com queijo ou presunto com queijo.','valor':'R$: 5,00'},
-
-{'nome':'Empadinha','descrision':'Frango','valor':'R$: 5,00'},
-
-{'nome':'Enrolado de salsicha','descrision':'','valor':'R$: 3,00'},
-
-{'nome':'Torta fria','descrision':'Pão de forma, patê de frango, requeijão, creme de leite, milho, ervilha e azeitona.','valor':'R$: 10,00'},
-
-{'nome':'Macarronada','descrision':'Carne com salsicha, barbecue, molho de tomate, milho, ervilha e azeitona.','valor':'R$: 8,00'},
-
-{'nome':'Empadão','descrision':'Patê de frango, requeijão e queijo cheddar.','valor':'R$: 10,00'},
-
-{'nome':'Panquecas','descrision':'Frango ou carne moída e queijo mussarela.','valor':'R$: 10,00'},
-
-{'nome':'Strogonoff de frango','descrision':'com arro    branco e batata palha','valor':'R$: 8,00'},
-
-{'nome':'vatapá de camarão','descrision':'com arroz branco','valor':'R$: 12,00'}
-
-]
+  {'nome':'Cachorro quente Especial ',
+  'descrision':'pão, 1 Salsicha, carne,molho,salada milho e ervilha, batalha palha, queijo ralado',
+  'valor':'R$: 8,00'},
+  
+  {'nome':'Super Cachorro quente ',
+  'descrision':'pão,2 salsicha  ,carne,molho, salada, milho, ervilha,  batalha palha, queijo mussarela , presunto, ',
+  'valor':'R$: 14,00'},
+  
+  {'nome':'Misto com ovo',
+  'descrision':'pão,ovo, presunto, queijo mussarela'
+  ,'valor':'R$: 9,00'},
+  
+  {'nome':'Misto sem ovo',
+  'descrision':'pão,presunto, queijo',
+  'valor':'R$: 7,00'},
+  
+  {'nome':'Brotinho Sabor Moda da casa ',
+  'descrision':'presunto ,queijo mussarela,  calabresa,  milho,ervilha,tomate, orégano,batalha palha',
+  'valor':'R$: 10,00'},
+  
+  {'nome':'Brotinho Sabor Mexicana',
+  'descrision':' presunto ,queijo mussarela,  bacon,  milho,ervilha,tomate,orégano, batalha palha, pimenta calabresa',
+  'valor':'R$: 10,00'},
+  
+  {'nome':'Brotinho Sabor frango',
+  'descrision':'Frango,queijo mussarela,orégano, tomate, milho,ervilha, batata palha'
+  ,'valor':'R$: 10,00'},
+  
+  {'nome':'Pastel',
+  'descrision':'Presunto  e queijo <br/>Carne com queijo <br>Frango com queijo <br>Queijo mussarela ',
+  'valor':'R$: 7,00'},
+  
+  {'nome':'Macarronada','descrision':'Carne com salsicha, barbecue, molho de tomate, milho, ervilha e azeitona.','valor':'R$: 8,00'},
+  
+  {'nome':'Empadão','descrision':'Patê de frango, requeijão e queijo cheddar.','valor':'R$: 10,00'},
+  
+  {'nome':'Panquecas','descrision':'Frango ou carne moída e queijo mussarela.','valor':'R$: 10,00'},
+  
+  {'nome':'Strogonoff de frango','descrision':'com arro    branco e batata palha','valor':'R$: 8,00'},
+  
+  {'nome':'vatapá de camarão','descrision':'com arroz branco','valor':'R$: 12,00'}
+  
+  ]
